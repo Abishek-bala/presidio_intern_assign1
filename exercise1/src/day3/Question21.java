cdba4c22a826555a744099cad61a3f04bec80a09
Abishek-bala <80381098+Abishek-bala@users.noreply.github.com>
2021-03-10 14:34:50 +0530

    Add files via upload

---
+++ exercise1/src/day3/Question21.java
@@ -0,0 +1,29 @@
+package day3;
+
+public class Question21 {
+	public static void main(String argv[]){ 
+	    /*int i; 
+	    int j; 
+	    outer: 
+	    for (i=1;i <3;i++) 
+	        inner: 
+	         for(j=1; j<3; j++) { 
+	                    if (j==2) 
+	                    continue outer; 
+	                    System.out.println("Value for i=" + i + " Value for j=" +j); 
+	                    } 
+	       
+		System.out.println(Math.floor(-4.7)); //-5.0
+		System.out.println(Math.round(-4.7)); //-5
+		System.out.println(Math.ceil(-4.7)); //-4.0
+		//System.out.println(Math.min(-4.7)); */
+		
+		Integer ten=new Integer(10); 
+		Long nine=new Long (9);
+		System.out.println(ten + nine); //19
+		int i=1;
+		System.out.println(i + ten);//11
+
+
+		} 
+}
