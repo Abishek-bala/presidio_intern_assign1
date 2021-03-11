e9fe378a17e34ac231c1e12abf1902d199b61005
Abishek-bala <80381098+Abishek-bala@users.noreply.github.com>
2021-03-11 14:58:02 +0530

    Add files via upload

--- exercise1/src/Day4/Question13.java
+++ exercise1/src/Day4/Question13.java
@@ -1,5 +1,26 @@
 package Day4;
 
 public class Question13 {
-
+	public static void main(String[] args) {
+		int[][] marks = {
+				{70,80,90},
+				{75,85,95},
+				{60,70,80},
+		};
+		String[] names = {"A","B", "C"};
+		int[] total = new int[marks.length];
+		for(int i=0;i<marks.length;i++) {
+			for(int j=0;j<marks[i].length;j++) {
+				total[i]+=marks[i][j];
+			}
+		}
+		System.out.println("Name\tmat`\tphy\tchem\ttot");
+		for(int i = 0;i<marks.length; i++) {
+			System.out.print(names[i]+"\t");
+			for(int j=0; j<marks[i].length; j++) {
+				System.out.print(marks[i][j]+"\t");
+			}
+			System.out.println(total[i]);
+		}
+	}
 }
---
+++ exercise1/src/Day4/Question14.java
@@ -0,0 +1,7 @@
+package Day4;
+
+public class Question14 {
+	public void main(String[] args) {
+		System.out.println("hello");
+	}
+}
