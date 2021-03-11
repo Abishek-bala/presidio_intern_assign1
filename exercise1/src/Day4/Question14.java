c5b8eb50d8abb780948293143964c808082a7cb4
Abishek-bala <80381098+Abishek-bala@users.noreply.github.com>
2021-03-11 14:59:58 +0530

    Add files via upload

--- exercise1/src/Day4/Question14.java
+++ exercise1/src/Day4/Question14.java
@@ -1,7 +1,9 @@
-package Day4;
-
 public class Question14 {
-	public void main(String[] args) {
+/**
+*Author = Abishek
+*date = 3/11/2021
+*/
+	public static void main(String[] args) {
 		System.out.println("hello");
 	}
 }
